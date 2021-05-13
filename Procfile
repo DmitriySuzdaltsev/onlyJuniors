c9b697bde60748b7be64c37ac98a3c1634dad79a
Суздальцев Дмитрий Сергеевич <Suzdaltsev.D.Se@sberbank.ru>
2021-05-13 18:16:01 +0300

    Back evolving

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: npm i && node ./nodeBackend.js $PORT
+web: cd ./ui && npm i && npm run build && cd && npm i && node ./nodeBackend.js $PORT
--- nodeBackend.js
+++ nodeBackend.js
@@ -19,7 +19,7 @@ app.get("/getStuff", (req, res) => {
 });
 
 app.get('/',function(req,res) {
-    res.sendFile('../build/index.html');
+    res.sendFile(__dirname + './build/index.html');
 });
 
 app.listen(port, () => {
