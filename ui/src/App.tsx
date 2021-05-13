4253d385b30ca101fbcdd21257578f7e9df6094b
Dymsy <orenmagic@protonmail.ch>
2021-05-13 20:21:48 +0300

    Heroku build please!

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -7,7 +7,7 @@ import axios from "axios";
 
 function App() {
   useEffect(() => {
-    axios.get("http://localhost:8080/getStuff").then((res) => console.log(res));
+    axios.get("http://localhost/getStuff").then((res) => console.log(res));
     console.log("trololo");
   }, []);
   return (
